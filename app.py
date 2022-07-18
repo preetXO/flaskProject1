842e8955ecdfa40e68e762a14482063616cc44e8
Preetpal <preetpa;singhspat@gmail.com>
2022-07-18 18:19:07 +0530

    added jinja 2

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/expressions/')
+def hello_world(): # put application's code here
+    color = 'brown'
+    animal_one = 'fox'
+    animal_two = 'dog'
+
+    orange_amount = 10
+    apple_amount = 20
+    donate_amount = 15
+
+    first_name = 'captain'
+    last_name = 'america'
+
+    kwargs = {
+        'color': color,
+        'animal_one': animal_one,
+        'animal_two': animal_two,
+        'orange_amount': orange_amount,
+        'apple_amount': apple_amount,
+        'donate_amount': donate_amount,
+        'first_name': first_name,
+        'last_name': last_name
+    }
+
+    return render_template('expressions.html', **kwargs)
 
 
 if __name__ == '__main__':
